799a896e1000c81b56439626a2f09e1e1511aefd
sobi-web <sobhan.parsamehr@gmail.com>
2025-10-15 04:03:37 +0330

    change user table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,19 +13,35 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+//            $table->string('first_name');
+//            $table->string('last_name');
+            $table->string('phone')->unique();
+            $table->timestamp('phone_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+         //   $table->rememberToken();
             $table->timestamps();
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('full_name')->nullable();
+            $table->string('job_title')->nullable();
+            $table->date('birth_date')->nullable();
+            $table->enum('gender', ['male','female','other'])->nullable();
+            $table->string('avatar_url')->nullable();
+            $table->text('bio')->nullable();
+            $table->timestamps();
         });
+
+
+
+//        Schema::create('password_reset_tokens', function (Blueprint $table) {
+//            $table->string('email')->primary();
+//            $table->string('token');
+//            $table->timestamp('created_at')->nullable();
+//        });
 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
@@ -43,7 +59,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('user_profiles');
+     //   Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
     }
 };

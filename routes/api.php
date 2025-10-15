c16df0d80c557cea9c81856df269d91146200934
sobi-web <sobhan.parsamehr@gmail.com>
2025-10-15 04:32:58 +0330

    add directory ApiV1 in api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+
+
+
+Route::prefix('v1')->group(function () {
+
+
+
+
+
+    Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+
+    Route::prefix('auth')->group(base_path('routes/ApiV1/auth.php'));
+
+
 });

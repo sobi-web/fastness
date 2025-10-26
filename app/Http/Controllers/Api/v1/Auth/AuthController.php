2a2b87ccca83695ff9017effed984de6839156fb
sobi-web <sobhan.parsamehr@gmail.com>
2025-10-27 02:50:53 +0330

    refactor Exception Otp

--- app/Http/Controllers/Api/v1/Auth/AuthController.php
+++ app/Http/Controllers/Api/v1/Auth/AuthController.php
@@ -2,42 +2,52 @@
 
 namespace App\Http\Controllers\Api\v1\Auth;
 
+use App\Exceptions\InvalidOtpCodeException;
 use App\Http\Controllers\BaseApiController;
 use App\Http\Requests\Api\V1\Auth\OtpVerifyRequest;
 use App\Services\OtpService;
 use App\Services\Userservice;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends BaseApiController
 {
     public function verifyOtp(OtpVerifyRequest $request, OtpService $otpService)
     {
-        $flow_token = $request->flow_token;
-        $code = $request->code;
-        $otp = $otpService->verify($flow_token, $code);
+        try {
+            $flow_token = $request->flow_token;
+            $code = $request->code;
+            $otp = $otpService->verify($flow_token, $code);
 
 
-        if ($otp === null) {
+            if ($otp === null) {
 
-            return $this->apiResponse(201, 'عملیات با خطا مواجه شد');
+                return $this->apiResponse(201, 'عملیات با خطا مواجه شد');
+
+            }
+
+            $user = Userservice::findOrCreateByPhone($otp->phone);
+
+            // صدور توکن Sanctum
+            $token = $user->createToken('fastness_api')->plainTextToken;
+
+            $otp->setAsVerified();
+
+            return $this->apiResponse(
+                true,
+                'ورود شما با موفقیت انجام شد ',
+                [
+                    'token' => $token,
+                    'user' => $user,
+                ]
+                , '200', '/dashboard');
+
+        }catch (InvalidOtpCodeException $e) {
+            throw ValidationException::withMessages([
+               'otp' => $e->getMessage(),
+            ]);
 
         }
-
-        $user = Userservice::findOrCreateByPhone($otp->phone);
-
-        // صدور توکن Sanctum
-        $token = $user->createToken('fastness_api')->plainTextToken;
-
-        $otp->setAsVerified();
-
-        return $this->apiResponse(
-            true,
-            'ورود شما با موفقیت انجام شد ',
-            [
-                'token' => $token,
-                'user' => $user,
-            ]
-            , '200', '/dashboard');
 
 
     }
@@ -45,7 +55,6 @@ class AuthController extends BaseApiController
 
     public function logout(Request $request) {
 
-         dd($request->user());
 
         $request->user()->currentAccessToken()->delete();
 
--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,6 @@ Route::prefix('v1')->middleware('json')->group(function () {
 
 
 
-    Route::prefix('auth')->group(base_path('routes/ApiV1/auth.php'))->middleware('guest');
-
+    Route::prefix('auth')->group(base_path('routes/ApiV1/auth.php'));
 
 });

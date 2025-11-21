38f5857885ab4a43170104c8542552843adca780
sobi-web <sobhan.parsamehr@gmail.com>
2025-11-22 02:26:38 +0330

    start dev course-comment

--- database/factories/Users/UserFactory.php
+++ database/factories/Users/UserFactory.php
@@ -25,9 +25,8 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'phone' => fake()->unique()->phoneNumber(),
+            'phone_verified_at' => now(),
             'remember_token' => Str::random(10),
         ];
     }
